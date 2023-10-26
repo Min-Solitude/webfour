199d253940006bddedd2710bd7bd72c828fcc786
Min solitude <121474260+Min-Solitude@users.noreply.github.com>
2023-10-26 17:47:31 +0700

    hú

---
+++ pages/index.tsx
@@ -0,0 +1,54 @@
+import { ConnectWallet, useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
+
+const Home: NextPage = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  const address = useAddress()
+  const contractAddress = '0xE7a589c0e601c87391C5ffEE33d74b8Be6D6CEA6'
+
+  const { contract } = useContract(contractAddress);
+
+  const { data: totalArt, isLoading: loadingTotalArts } = useContractRead(contract, 'getCampaigns')
+
+  const handleCheckMobile = () => {
+    if (window.innerWidth < 768) {
+      setIsMobile(true)
+    } else {
+      setIsMobile(false)
+    }
+  }
+
+  useEffect(() => {
+    handleCheckMobile()
+    window.addEventListener('resize', handleCheckMobile)
+    return () => {
+      window.removeEventListener('resize', handleCheckMobile)
+    }
+  }, [])
+
+  if (!isMobile) {
+    return (
+      <div className="">Chúng tôi không hỗ trợ màn hình này</div>
+    )
+  }
+
+  return (
+    <div>
+      <div >
+        <ConnectWallet
+          dropdownPosition={{
+            side: "bottom",
+            align: "center",
+          }}
+        />
+        <span className="text-[0.9rem] font-bold text-red-600">
+          {totalArt?.toString()} 🎨
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
